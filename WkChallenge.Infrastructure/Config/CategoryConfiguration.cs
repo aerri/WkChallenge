06115ab3e0c60af103d4cded157e10558eb0fd3a
Alex Rocha <alex.sirocha@outlook.com>
2022-09-11 19:49:50 -0300

    Add EntityFramework configuration files

---
+++ WkChallenge.Infrastructure/Config/CategoryConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using WkChallenge.Core.Models;
+
+namespace WkChallenge.Infrastructure.Config;
+
+public class CategoryConfiguration : IEntityTypeConfiguration<Category>
+{
+	public void Configure(EntityTypeBuilder<Category> builder)
+	{
+		builder.ToTable("Categories").HasKey(category => category.Id);
+	}
+}
---
+++ WkChallenge.Infrastructure/Data/AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using System.Reflection;
+using Microsoft.EntityFrameworkCore;
+using WkChallenge.Core.Models;
+
+namespace WkChallenge.Infrastructure.Data;
+
+public class AppDbContext : DbContext
+{
+	public DbSet<Product>? Products { get; set; }
+
+	public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+	{
+	}
+
+	protected override void OnModelCreating(ModelBuilder modelBuilder)
+	{
+		base.OnModelCreating(modelBuilder);
+		modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+		modelBuilder.UseSerialColumns();
+	}
+}
