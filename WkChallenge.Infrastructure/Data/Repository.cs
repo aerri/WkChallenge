197e4d1ae0f3ada354282700432d2299658c19d7
Alex Rocha <alex.sirocha@outlook.com>
2022-09-11 20:02:13 -0300

    Add Repository implementation to WkChallenge.Infrastructure

---
+++ WkChallenge.Infrastructure/Data/Repository.cs
@@ -0,0 +1,11 @@
+﻿using Ardalis.Specification.EntityFrameworkCore;
+using WkChallenge.Core.Interfaces;
+
+namespace WkChallenge.Infrastructure.Data;
+
+public class Repository<T> : RepositoryBase<T>, IRepository<T> where T : class, IAggregateRoot
+{
+	public Repository(AppDbContext context) : base(context)
+	{
+	}
+}
