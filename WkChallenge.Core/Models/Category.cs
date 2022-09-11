64ecae949962b06b74bc38ddc093f5816386ed8b
Alex Rocha <alex.sirocha@outlook.com>
2022-09-11 19:44:14 -0300

    Add Category class to WkChallenge.Core.Models

--- WkChallenge.Core/Models/Category.cs
+++ WkChallenge.Core/Models/Category.cs
@@ -1,8 +1,6 @@
 ﻿namespace WkChallenge.Core.Models;
 
-public record Product : BaseModel<int>
+public record Category : BaseModel<int>
 {
 	public string Name { get; set; } = string.Empty;
-
-	public Category Category { get; set; } = new();
 }
