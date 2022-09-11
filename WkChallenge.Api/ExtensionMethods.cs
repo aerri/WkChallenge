8df4c84f2d41041f0a74386b74d0b65b2b82c5c2
Alex Rocha <alex.sirocha@outlook.com>
2022-09-11 20:05:40 -0300

    Add ExtensionMethods class with MigrateDatabase method to WkChallenge.Api project

---
+++ WkChallenge.Api/ExtensionMethods.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WkChallenge.Infrastructure.Data;
+
+namespace WkChallenge.Api;
+
+public static class ExtensionMethods
+{
+	public static void MigrateDatabase(this WebApplication webApp)
+	{
+		using var scope = webApp.Services.CreateScope();
+		using var appContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+		appContext.Database.Migrate();
+	}
+}
