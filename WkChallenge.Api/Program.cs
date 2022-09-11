878c9169c46b0d722e57303b0a709ebea02afb1e
Alex Rocha <alex.sirocha@outlook.com>
2022-09-11 20:04:14 -0300

    Add WkChallenge.Api project

---
+++ WkChallenge.Api/Program.cs
@@ -0,0 +1,30 @@
+using Microsoft.EntityFrameworkCore;
+using WkChallenge.Api;
+using WkChallenge.Core.Interfaces;
+using WkChallenge.Core.Models;
+using WkChallenge.Infrastructure.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddControllers();
+builder.Services.AddDbContext<AppDbContext>(options => options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddTransient<IRepository<Category>, Repository<Category>>();
+builder.Services.AddTransient<IRepository<Product>, Repository<Product>>();
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+	app.UseSwagger();
+	app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseRouting();
+app.UseEndpoints(endpoints => endpoints.MapControllers());
+app.MigrateDatabase();
+app.Run();
