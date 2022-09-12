167a9d466880b3402d7a87644d410444cd4f503d
Alex Rocha <alex.sirocha@outlook.com>
2022-09-12 15:41:21 -0300

    Refactor base ViewModels

--- WkChallenge.Web.Shared/ViewModels/BaseMessage.cs
+++ WkChallenge.Web.Shared/ViewModels/BaseMessage.cs
@@ -3,6 +3,5 @@
 public abstract class BaseMessage
 {
 	protected Guid _correlationId = Guid.NewGuid();
-
 	public Guid CorrelationId => _correlationId;
 }
--- WkChallenge.Web.Shared/ViewModels/BaseResponse.cs
+++ WkChallenge.Web.Shared/ViewModels/BaseResponse.cs
@@ -2,7 +2,6 @@
 
 public abstract class BaseResponse : BaseMessage
 {
-	protected BaseResponse(Guid correlationId) { base._correlationId = correlationId; }
-
+	protected BaseResponse(Guid correlationId) { _correlationId = correlationId; }
 	protected BaseResponse() { }
 }
