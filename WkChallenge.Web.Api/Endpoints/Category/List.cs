b93ad463d48d64492f634784ce1f13cd7a1c0d5d
Alex Rocha <alex.sirocha@outlook.com>
2022-09-12 15:55:22 -0300

    Add "List" handler for "Category" entity to WkChallenge.Web.Api

---
+++ WkChallenge.Web.Api/Endpoints/Category/List.cs
@@ -0,0 +1,30 @@
+﻿using Ardalis.ApiEndpoints;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
+using WkChallenge.Core.Interfaces;
+using WkChallenge.Web.Shared.ViewModels.Category;
+
+namespace WkChallenge.Web.Api.Endpoints.Category;
+
+public class List : EndpointBaseAsync.WithRequest<ListCategoriesRequest>.WithActionResult<ListCategoriesResponse>
+{
+	private readonly IRepository<Core.Models.Category> _repository;
+	private readonly IMapper _mapper;
+
+	public List(IRepository<Core.Models.Category> repository, IMapper mapper)
+	{
+		_repository = repository;
+		_mapper = mapper;
+	}
+
+	[HttpGet("api/categories")]
+	[SwaggerOperation(Summary = "List Categories", Description = "List Categories", OperationId = "categories.list", Tags = new[] {"CategoryEndpoints"})]
+	public override async Task<ActionResult<ListCategoriesResponse>> HandleAsync([FromQuery] ListCategoriesRequest request, CancellationToken cancellationToken = new())
+	{
+		var response = new ListCategoriesResponse(request.CorrelationId);
+		var categories = await _repository.ListAsync(cancellationToken);
+		response.Categories = _mapper.Map<List<CategoryDto>>(categories);
+		return Ok(response);
+	}
+}
