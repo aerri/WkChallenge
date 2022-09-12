2be1e379e1b5c29f9dc306aa079e71970a4e2510
Alex Rocha <alex.sirocha@outlook.com>
2022-09-12 15:48:35 -0300

    Add models used by "GetById" handler of "Category" entity

---
+++ WkChallenge.Web.Shared/ViewModels/Category/GetByIdCategoryRequest.cs
@@ -0,0 +1,6 @@
+﻿namespace WkChallenge.Web.Shared.ViewModels.Category;
+
+public class GetByIdCategoryRequest : BaseRequest
+{
+	public int Id { get; set; }
+}
---
+++ WkChallenge.Web.Shared/ViewModels/Category/GetByIdCategoryResponse.cs
@@ -0,0 +1,7 @@
+﻿namespace WkChallenge.Web.Shared.ViewModels.Category;
+
+public class GetByIdCategoryResponse : BaseResponse
+{
+	public CategoryDto Category { get; set; } = new();
+	public GetByIdCategoryResponse(Guid correlationId) : base(correlationId) { }
+}
