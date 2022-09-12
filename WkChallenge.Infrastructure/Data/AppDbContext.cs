a289773def16130c16705d94069621ccf00c245e
Alex Rocha <alex.sirocha@outlook.com>
2022-09-12 15:32:38 -0300

    Add "Categories" DbSet

--- WkChallenge.Infrastructure/Data/AppDbContext.cs
+++ WkChallenge.Infrastructure/Data/AppDbContext.cs
@@ -1,16 +1,13 @@
-using System.Reflection;
+﻿using WkChallenge.Core.Models;
 using Microsoft.EntityFrameworkCore;
-using WkChallenge.Core.Models;
 
 namespace WkChallenge.Infrastructure.Data;
 
 public class AppDbContext : DbContext
 {
+	public DbSet<Category>? Categories { get; set; }
 	public DbSet<Product>? Products { get; set; }
-
-	public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
-	{
-	}
+	public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 
 	protected override void OnModelCreating(ModelBuilder modelBuilder)
 	{
