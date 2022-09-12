a07b6f2e368350d1bd40054432f2d8355417b605
Alex Rocha <alex.sirocha@outlook.com>
2022-09-12 15:54:08 -0300

    Add "Create" handler for "Category" entity to WkChallenge.Web.Api

---
+++ WkChallenge.Web.Api/Endpoints/Category/Create.cs
@@ -0,0 +1,32 @@
+﻿using Ardalis.ApiEndpoints;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
+using WkChallenge.Core.Interfaces;
+using WkChallenge.Web.Shared.ViewModels.Category;
+
+namespace WkChallenge.Web.Api.Endpoints.Category;
+
+public class Create : EndpointBaseAsync.WithRequest<CreateCategoryRequest>.WithActionResult<CreateCategoryResponse>
+{
+	private readonly IRepository<Core.Models.Category> _repository;
+	private readonly IMapper _mapper;
+
+	public Create(IRepository<Core.Models.Category> repository, IMapper mapper)
+	{
+		_repository = repository;
+		_mapper = mapper;
+	}
+
+	[HttpPost("api/categories")]
+	[SwaggerOperation(Summary = "Creates a new Category", Description = "Creates a new Category", OperationId = "categories.create", Tags = new[] {"CategoryEndpoints"})]
+	public override async Task<ActionResult<CreateCategoryResponse>> HandleAsync(CreateCategoryRequest request, CancellationToken cancellationToken = new())
+	{
+		var response = new CreateCategoryResponse(request.CorrelationId);
+		var toAdd = _mapper.Map<Core.Models.Category>(request);
+		toAdd = await _repository.AddAsync(toAdd, cancellationToken);
+		var dto = _mapper.Map<CategoryDto>(toAdd);
+		response.Category = dto;
+		return Ok(response);
+	}
+}
