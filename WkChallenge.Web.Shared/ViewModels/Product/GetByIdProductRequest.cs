514d3f48f4e311f06ff1b78f0a71a4949b58031e
Alex Rocha <alex.sirocha@outlook.com>
2022-09-14 15:19:39 -0300

    Add models used by "GetById" handler of  "Product" entity

---
+++ WkChallenge.Web.Shared/ViewModels/Product/GetByIdProductRequest.cs
@@ -0,0 +1,6 @@
+﻿namespace WkChallenge.Web.Shared.ViewModels.Product;
+
+public class GetProductRequest : BaseRequest
+{
+	public int ProductId { get; set; }
+}
---
+++ WkChallenge.Web.Shared/ViewModels/Product/GetByIdProductResponse.cs
@@ -0,0 +1,7 @@
+﻿namespace WkChallenge.Web.Shared.ViewModels.Product;
+
+public class GetProductResponse : BaseResponse
+{
+	public ProductDto Product { get; set; } = new();
+	public GetProductResponse(Guid correlationId) : base(correlationId) { }
+}
