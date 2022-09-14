6fc46ba71b79e35e766fa295bdf861a17c5179e1
Alex Rocha <alex.sirocha@outlook.com>
2022-09-14 13:09:00 -0300

    Fix/Add configurations in WkChallange.Web.Api/Program.cs file

--- WkChallenge.Web.Api/Program.cs
+++ WkChallenge.Web.Api/Program.cs
@@ -1,30 +1,31 @@
-using System.Reflection;
-using Microsoft.EntityFrameworkCore;
 using WkChallenge.Web.Api;
+using Microsoft.OpenApi.Models;
 using WkChallenge.Core.Interfaces;
-using WkChallenge.Core.Models;
+using Microsoft.EntityFrameworkCore;
 using WkChallenge.Infrastructure.Data;
+using WkChallenge.Web.Api.MappingProfiles;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+							   {
+								   options.SwaggerDoc("v1", new OpenApiInfo {Title = "WkChallenge.Web.Api", Version = "v1"});
+								   options.EnableAnnotations();
+								   options.OperationFilter<SwaggerExtensions.CustomOperationFilters>();
+								   options.CustomSchemaIds(type => type.Name.Replace("Dto", ""));
+							   });
 builder.Services.AddControllers();
-builder.Services.AddAutoMapper(Assembly.GetExecutingAssembly());
+builder.Services.AddAutoMapper(typeof(CategoryProfile).Assembly, typeof(ProductProfile).Assembly);
 builder.Services.AddDbContext<AppDbContext>(options => options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
-builder.Services.AddTransient<IRepository<Category>, Repository<Category>>();
-builder.Services.AddTransient<IRepository<Product>, Repository<Product>>();
+builder.Services.AddTransient(typeof(IRepository<>), typeof(Repository<>));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-	app.UseSwagger();
-	app.UseSwaggerUI();
-}
-
+app.UseSwagger();
+app.UseSwaggerUI();
 app.UseHttpsRedirection();
 app.UseRouting();
 app.UseEndpoints(endpoints => endpoints.MapControllers());
---
+++ WkChallenge.Web.Api/SwaggerExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace WkChallenge.Web.Api;
+
+public static class SwaggerExtensions
+{
+	public class CustomOperationFilters : IOperationFilter
+	{
+		public void Apply(OpenApiOperation operation, OperationFilterContext context)
+		{
+			foreach (var parameter in operation.Parameters)
+				if ("CorrelationId".Equals(parameter.Name))
+					parameter.Required = false;
+		}
+	}
+}
