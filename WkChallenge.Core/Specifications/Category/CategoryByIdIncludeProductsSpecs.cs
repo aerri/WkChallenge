c6c632610c4bafeb6363852148bde386a9e5b328
Alex Rocha <alex.sirocha@outlook.com>
2022-09-14 13:25:56 -0300

    Add specification to query "Category" entities

---
+++ WkChallenge.Core/Specifications/Category/CategoryByIdIncludeProductsSpecs.cs
@@ -0,0 +1,11 @@
+﻿using Ardalis.Specification;
+
+namespace WkChallenge.Core.Specifications.Category;
+
+public sealed class CategoryByIdIncludeProductsSpecs : SingleResultSpecification<Models.Category>
+{
+	public CategoryByIdIncludeProductsSpecs(int categoryId)
+	{
+		Query.Include(category => category.Products).Where(category => category.Id == categoryId);
+	}
+}
