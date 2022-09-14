c9ec1117389bf8cf69a6ef63a0daf02782c3e8fe
Alex Rocha <alex.sirocha@outlook.com>
2022-09-14 13:26:23 -0300

    Add mapping profile for the "Product" entity

---
+++ WkChallenge.Web.Api/MappingProfiles/ProductProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using WkChallenge.Core.Models;
+using WkChallenge.Web.Shared.ViewModels.Product;
+
+namespace WkChallenge.Web.Api.MappingProfiles;
+
+public class ProductProfile : Profile
+{
+	public ProductProfile()
+	{
+		CreateMap<Product, ProductDto>();
+		CreateMap<CreateProductRequest, Product>();
+	}
+}
