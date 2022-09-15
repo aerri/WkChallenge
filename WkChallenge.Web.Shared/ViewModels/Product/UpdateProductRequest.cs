74ac60afc1dae98cb80787ae23fbcac2f411ad8b
Alex Rocha <alex.sirocha@outlook.com>
2022-09-15 10:28:43 -0300

    Add models used by "Update" handler of  "Product" entity

---
+++ WkChallenge.Web.Shared/ViewModels/Product/UpdateProductRequest.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WkChallenge.Web.Shared.ViewModels.Product;
+
+public class UpdateProductRequest : BaseRequest
+{
+	[Range(1, int.MaxValue)]
+	public int Id { get; set; }
+
+	[StringLength(int.MaxValue, MinimumLength = 1)]
+	public string Name { get; set; } = string.Empty;
+
+	[Range(1, int.MaxValue)]
+	public int CategoryId { get; set; }
+}
---
+++ WkChallenge.Web.Shared/ViewModels/Product/UpdateProductResponse.cs
@@ -0,0 +1,7 @@
+﻿namespace WkChallenge.Web.Shared.ViewModels.Product;
+
+public class UpdateProductResponse : BaseResponse
+{
+	public ProductDto Product { get; set; } = new();
+	public UpdateProductResponse(Guid correlationId) : base(correlationId) { }
+}
